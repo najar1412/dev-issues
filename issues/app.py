8c078fb2621b683e97401181ccc85a6c67849b1a
najar1412 <rory.jarrel@gmail.com>
2017-02-04 12:43:06 -0500

    ui - project, issues

--- issues/app.py
+++ issues/app.py
@@ -3,12 +3,27 @@ import requests
 
 app = Flask(__name__)
 
+BASEURL = 'http://127.0.0.1:5050/issues/api'
 
 @app.route('/', methods=['GET'])
 def index():
-
     return render_template('home.html')
 
+
+@app.route('/project', methods=['GET'])
+def project():
+
+    r = requests.get('{}/project'.format(BASEURL))
+
+    return render_template('project.html', project=r.json())
+
+
+@app.route('/issue', methods=['GET'])
+def issue():
+
+    r = requests.get('{}/issue'.format(BASEURL))
+
+    return render_template('issue.html', issue=r.json())
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
